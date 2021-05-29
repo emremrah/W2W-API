4c1e14ad4f55c36697901a52156542fe8f7bc7a7
emremrah <emre@hiddenslate.com>
2021-05-29 21:27:42 +0300

    Add pop100 endpoint, update requirements

--- wtw/app.py
+++ wtw/app.py
@@ -1,3 +1,47 @@
+from typing import List
+
+from flask import jsonify, request
+from imdb import IMDb
+from imdb.Movie import Movie
+
 from . import create_app
+from .models import MovieModel, Top100Request
 
 app = create_app()
+
+ia = IMDb()
+
+
+@app.route('/pop100', methods=['POST'])
+def get_top_100():
+    req = Top100Request.Request(**request.get_json())
+    edible_movies = []
+
+    # get 100 most popular movies
+    top100: List[Movie] = ia.get_popular100_movies()
+
+    # limit the number of movies to be searched by zipping the loop
+    for _, movie in zip(range(req.search_in), top100):
+        movie: Movie = ia.get_movie(movie.getID(), info=('main', 'genre'))
+
+        genres = [genre.lower()
+                  for genre in movie.get('genre') if genre is not None]
+
+        # if any of the user's desired genres are not in the movie's, skip this
+        # movie
+        if not any([genre.lower() in genres for genre in req.genres]):
+            continue
+
+        # if movie's rating is below minimum rating, skip this movie
+        if movie.get('rating', 0) < req.min_rating:
+            continue
+
+        # create a movie model and add it to "edible" movie list
+        movie = MovieModel(id=movie.getID(),
+                           title=movie.get('title'),
+                           plot=movie.get('plot'),
+                           rating=movie.get('rating'),
+                           genres=movie.get('genre'))
+        edible_movies.append(movie.dict())
+
+    return jsonify(edible_movies)
