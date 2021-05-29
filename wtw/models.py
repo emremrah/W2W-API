27c290b89f12c071fe801c12f404b138f5b8eb06
emremrah <emre@hiddenslate.com>
2021-05-29 21:24:00 +0300

    Create data models

---
+++ wtw/models.py
@@ -0,0 +1,18 @@
+from typing import List, Optional, Union
+
+from pydantic import BaseModel
+
+
+class Top100Request:
+    class Request(BaseModel):
+        genres: List[str]
+        min_rating: Union[int, float] = 0
+        search_in: int = 100
+
+
+class MovieModel(BaseModel):
+    id: str
+    title: str
+    plot: Optional[str]
+    rating: Optional[float]
+    genres: Optional[List[str]]
