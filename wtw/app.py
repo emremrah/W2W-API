ac3e08ba796319c8a519e29a0f21c763fc46e2ef
Emre Emrah <emre95@gmail.com>
2021-06-02 22:29:26 +0300

    Add genres endpoint

--- wtw/app.py
+++ wtw/app.py
@@ -4,6 +4,8 @@ from flask import jsonify, request
 from imdb import IMDb
 from imdb.Movie import Movie
 
+from wtw.assets.genres import GENRES
+
 from . import create_app
 from .models import MovieModel, Top100Request
 
@@ -17,7 +19,8 @@ def filter_top100(top100: List[Movie], min_rating: int, genres: List[str], searc
 
     # limit the number of movies to be searched by zipping the loop
     for _, movie in zip(range(search_in), top100):
-        movie: Movie = ia.get_movie(movie.getID(), info=('main', 'genre'))
+        movie: Movie = ia.get_movie(
+            movie.getID(), info=('main', 'plot', 'genre'))
 
         movie_genres = [genre.lower()
                         for genre in movie.get('genre') if genre is not None]
@@ -34,9 +37,9 @@ def filter_top100(top100: List[Movie], min_rating: int, genres: List[str], searc
         # create a movie model and add it to "edible" movie list
         movie = MovieModel(id=movie.getID(),
                            title=movie.get('title'),
-                           plot=movie.get('plot'),
+                           plot=movie.get('plot outline', ''),
                            rating=movie.get('rating'),
-                           genres=movie.get('genre'))
+                           genres=movie.get('genre', []))
         edible_movies.append(movie.dict())
 
     return edible_movies
@@ -52,3 +55,8 @@ def get_top_100():
     edible_movies = filter_top100(
         top100, req.min_rating, req.genres, req.search_in)
     return jsonify(edible_movies)
+
+
+@app.route('/genres', methods=['GET'])
+def get_genres():
+    return jsonify(sorted(list(GENRES.keys())))
