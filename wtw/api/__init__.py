29557a0fadb2819aa7fc0e6c90de9162bdf74ec1
Emre Emrah <emre95@gmail.com>
2023-12-27 13:52:05 +0300

    Add flask cors to allow all hosts

--- wtw/api/__init__.py
+++ wtw/api/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS
 
 from wtw import __version__
 
@@ -8,6 +9,8 @@ from wtw import __version__
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(SECRET_KEY='dev')
+
+    CORS(app)
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
